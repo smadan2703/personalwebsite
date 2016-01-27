bccb495c3923923b3b9c2ba716d07aa7dea89673
Madan kumar <smadan2703@gmail.com>
2016-01-27 18:04:02 +0000

    contact.php edited online with Bitbucket

--- contact.php
+++ contact.php
@@ -12,14 +12,12 @@ if(isset($_POST))
 		
 		/* starting the email message */
 	$name = $_POST['name'];
-	$fr = $_POST['email'];
-	$sub = $_POST['subject'];
 	$email = $_POST['email'];
 	$comments = $_POST['comments'];
 	$to = "smadan2703@gmail.com"; // your email address
-	$subject = "Email from MK Site : $sub";
+	$subject = "Email from MK Site";
 	$message = "First Name : $name\n\nEmail ID : $email\n\nComments : $comments";
-	$from = "smadan2703@gmail.com";
+
 	if(mail($to,$subject,$message,"From:" . $email))
 		{
 		echo "Email was sent";
