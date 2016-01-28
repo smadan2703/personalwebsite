da53dc804d41ce61fa83a35f55caea5bfd59471a
Madan kumar <smadan2703@gmail.com>
2016-01-28 12:10:58 +0000

    contact.php edited online with Bitbucket

--- contact.php
+++ contact.php
@@ -8,11 +8,7 @@ if(isset($_POST))
 		$key=$field; //putting the response into variables
 		}
 		
-$dbhost = "rattic-db1.cmyjlrhcajvb.ap-southeast-1.rds.amazonaws.com";
-$dbuser = "rattic";
-$dbpass = "vine2013M";
-$dbname = "ratticdb";
-$dt = "27-mar-2016";
+
 
 	$name = $_POST['name'];
 	$email = $_POST['email'];
