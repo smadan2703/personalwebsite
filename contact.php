ceef4f44849683a3358030b99cd5575b0665c5fe
Madan kumar <smadan2703@gmail.com>
2016-05-02 09:52:08 +0000

    Working email properly

--- contact.php
+++ contact.php
@@ -1,4 +1,6 @@
 <?php
+
+/* checking if data was sent */
 if(isset($_POST))
 {
 	foreach($_POST as $key => $field)
@@ -8,20 +10,18 @@ if(isset($_POST))
 		$key=$field; //putting the response into variables
 		}
 		
+		/* starting the email message */
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$comments = $_POST['comments'];
 	$to = "smadan2703@gmail.com"; // your email address
 	$subject = "Email from MK Site";
 	$message = "First Name : $name\n\nEmail ID : $email\n\nComments : $comments";
-	
-	
+
 	if(mail($to,$subject,$message,"From:" . $email))
 		{
 		echo "Email was sent";
 		}
- 
-
 }
 
 
